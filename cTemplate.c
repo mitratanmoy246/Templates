097e1c164e528b9e76725014a079dd003cd93388
Tanmoy Mitra <your-email@example.com>
2026-06-13 11:25:28 +0600

    updated templates

---
+++ cTemplate.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+#include <ctype.h>
+
+void solve()
+{
+    
+}
+
+int main()
+{
+    int t = 1;
+    // scanf("%d", &t); // uncomment for multiple test cases
+
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
